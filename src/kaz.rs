0943c47144e5bf81f35b5d2eb4c0c4cf1bcd4a12
berzi <anon>
2024-07-09 22:06:03 +0530

    .

---
+++ src/kaz.rs
@@ -0,0 +1,14 @@
+/// adds one to the number fiven
+///
+///
+/// # Exampes
+/// ```
+///lick with ttylenol
+/// ```
+///
+/// what the heck is goijg n
+///
+///
+pub fn add_one(x: i32) -> i32{
+    return x;
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+mod kaz;
+fn main() {
+    println!("Hello, world!");
+}
